1465e7af83e95e504a3b5085adeb1e5316cd8301
Andre Cleary <coderandrecleary@gmail.com>
2019-01-28 14:08:39 -0500

    changes made

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,14 +3,14 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelcetorAll(".nav-item");
+const navItems = document.querySelectorAll(".nav-item");
 
 //Set the initial state of Menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
 
-function toggle() {
+function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
